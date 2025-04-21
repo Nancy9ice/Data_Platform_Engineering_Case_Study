23998d6061a7031d82913261e12e89b63654ca72
Bee0933 <bestnyah7@gmail.com>
2025-04-21 10:16:04 +0100

    add: mwaa metrics alerts
    Signed-off-by: Bee0933 <bestnyah7@gmail.com>

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -30,7 +30,7 @@ resource "aws_cloudwatch_metric_alarm" "builditall_s3_bucket_4xx_errors" {
   dimensions = {
     BucketName = aws_s3_bucket.builditall_secure_bucket.bucket
   }
-  alarm_description = "Too many 4xx errors"
+  alarm_description = "BuildItAll-S3: Too many 4xx errors"
   alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
 }
 
@@ -46,7 +46,7 @@ resource "aws_cloudwatch_metric_alarm" "s3_bucket_5xx_errors" {
   dimensions = {
     BucketName = aws_s3_bucket.builditall_secure_bucket.bucket
   }
-  alarm_description = "Too many 5xx errors"
+  alarm_description = "BuildItAll-S3: Too many 5xx errors"
   alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
 }
 
@@ -63,7 +63,75 @@ resource "aws_cloudwatch_metric_alarm" "builditall_s3_high_latency" {
   dimensions = {
     BucketName = aws_s3_bucket.builditall_secure_bucket.bucket
   }
-  alarm_description = "S3 latency exceeds threshold (500ms)"
+  alarm_description = "BuildItAll-S3: latency exceeds threshold (500ms)"
   alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
 }
 
+# MwAA Alerts
+# Task Instances Failed
+resource "aws_cloudwatch_metric_alarm" "builditall_mwaa_task_instances_failed" {
+  alarm_name          = "${var.project}-${var.env_prefix}-MWAATaskInstancesFailed"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "TaskInstancesFailed"
+  namespace           = "AWS/MWAA"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 1
+  dimensions = {
+    EnvironmentName = aws_mwaa_environment.builditall_mwaa_env.name
+  }
+  alarm_description = "BuildItAll-MWAA: One or more Airflow task instances failed"
+  alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
+}
+
+# Scheduler Lag
+resource "aws_cloudwatch_metric_alarm" "builditall_mwaa_scheduler_lag" {
+  alarm_name          = "${var.project}-${var.env_prefix}-MWAASchedulerLag"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "SchedulerLag"
+  namespace           = "AWS/MWAA"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 60 # seconds
+  dimensions = {
+    EnvironmentName = aws_mwaa_environment.builditall_mwaa_env.name
+  }
+  alarm_description = "BuildItAll-MWAA: Scheduler lag exceeds 60 seconds"
+  alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
+}
+
+# Web Server Latency
+resource "aws_cloudwatch_metric_alarm" "builditall_mwaa_webserver_latency" {
+  alarm_name          = "${var.project}-${var.env_prefix}-MWAAWebServerLatency"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "WebServerLatency"
+  namespace           = "AWS/MWAA"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 1000 # ms
+  dimensions = {
+    EnvironmentName = aws_mwaa_environment.builditall_mwaa_env.name
+  }
+  alarm_description = "MWAA: Web server latency exceeds 1000ms"
+  alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
+}
+
+# Environment Health
+resource "aws_cloudwatch_metric_alarm" "builditall_mwaa_environment_health" {
+  alarm_name          = "${var.project}-${var.env_prefix}-MWAAEnvironmentHealth"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "EnvironmentHealth"
+  namespace           = "AWS/MWAA"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 1 # healthy = 1, unhealthy = 0
+  dimensions = {
+    EnvironmentName = aws_mwaa_environment.builditall_mwaa_env.name
+  }
+  alarm_description = "BuildItAll-MWAA: Environment health is degraded"
+  alarm_actions     = [aws_sns_topic.builditall_alerts.arn]
+}
