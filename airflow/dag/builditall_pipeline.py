e3f3c8486aefea4b327c8a92e4efa4de892fd47c
tdadadavid <davidtofunmidada@gmail.com>
2025-04-26 19:18:41 +0100

    fix flake8 issues
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- airflow/dag/builditall_pipeline.py
+++ airflow/dag/builditall_pipeline.py
@@ -19,7 +19,8 @@ INPUT_PATH = "s3://your-bucket-name/input-data/"
 OUTPUT_PATH = "s3://your-bucket-name/processed-data/"
 
 # DAG definition for processing sensor data
-# This pipeline reads raw sensor data, applies schema definitions, processes it,
+# This pipeline reads raw sensor data, applies schema
+# definitions, processes it,
 # transforms it, and writes the output to an S3 bucket.
 with DAG(
     dag_id="sensor_data_processing_pipeline",
@@ -29,26 +30,29 @@ with DAG(
     default_args={"owner": "airflow", "retries": 1},
 ) as dag:
 
-    # 
+    #
     # Task 1: Start a Spark session using the create_spark_session function
     task_start_spark = PythonOperator(
         task_id="start_spark",
         python_callable=create_spark_session,
     )
 
-    # Task 2: Define the schema for sensor data using the define_schema function
+    # Task 2: Define the schema for sensor data using the define_schema
+    # function
     task_define_schema = PythonOperator(
         task_id="define_schema",
         python_callable=define_schema,
     )
 
-    # Task 3: Process the raw sensor data to clean and structure it using the process_raw_data function
+    # Task 3: Process the raw sensor data to clean and structure it using the
+    # process_raw_data function
     task_process_raw_data = PythonOperator(
         task_id="process_raw_data",
         python_callable=process_raw_data,
     )
 
-    # Task 4: Read the raw data from the input path using the read_raw_data function
+    # Task 4: Read the raw data from the input path using the read_raw_data
+    # function
     task_read_data = PythonOperator(
         task_id="read_data",
         python_callable=read_raw_data,
@@ -60,14 +64,17 @@ with DAG(
         python_callable=transform_data,
     )
 
-    # Task 6: Write the transformed data to the output path using the write_data function
+    # Task 6: Write the transformed data to the output path using the
+    # write_data function
     task_write_raw_data = PythonOperator(
         task_id="write_raw_data",
         python_callable=write_data,
     )
 
-    # Set task dependencies to define the sequence of execution for the pipeline
-    # The tasks are organized in a way that ensures proper data flow and processing
+    # Set task dependencies to define the sequence
+    # of execution for the pipeline. The tasks are
+    # organized in a way that ensures proper data flow
+    # and processing
     (
         task_start_spark
         >> task_read_data
